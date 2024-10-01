66b3f524e01e4fe61a4b74de75a93e99c07c0aff
Mikuto <mikuto1227k@gmail.com>
2024-10-01 15:06:08 +0900

    ref:練習外_コードのリファクタリングをする

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,12 +5,15 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+
+# キーバインドと移動量の定義
 DELTA = {
     pg.K_UP: (0,-5),
     pg.K_DOWN: (0,+5),
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0),
 }
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def check_bound(obj_rct:pg.Rect):
@@ -27,46 +30,50 @@ def check_bound(obj_rct:pg.Rect):
     return yoko,tate
 
 def main():
+    # 画面初期化
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+
+    bg_img = pg.image.load("fig/pg_bg.jpg")  # 背景画像読み込み
+
+    # こうかとん初期化
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    # 爆弾初期化
     bb_img = pg.Surface((20,20))
     bb_img.set_colorkey((0,0,0)) # 爆弾の背景画像を透過する
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
-    vx,vy = +5,+5
+    vx,vy = +5,+5  # 爆弾の移動量初期化
+
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+        
+        screen.blit(bg_img, [0, 0])  # 背景描画
 
-        key_lst = pg.key.get_pressed()
+        # キー操作読み取り部
+        key_lst = pg.key.get_pressed()  # キー操作読み取り
         sum_mv = [0, 0]
-
         for k in DELTA:
             if key_lst[k]:
                 sum_mv[0] += DELTA[k][0]
                 sum_mv[1] += DELTA[k][1]
-        # if key_lst[pg.K_UP]:
-        #     sum_mv[1] -= 5
-        # if key_lst[pg.K_DOWN]:
-        #     sum_mv[1] += 5
-        # if key_lst[pg.K_LEFT]:
-        #     sum_mv[0] -= 5
-        # if key_lst[pg.K_RIGHT]:
-        #     sum_mv[0] += 5
+        
+        # こうかとんの移動反映
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True,True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
 
+        # 爆弾の移動反映
         bb_rct.move_ip(vx,vy)
         yoko, tate = check_bound(bb_rct)
         if not yoko:
@@ -74,6 +81,7 @@ def main():
         if not tate:
             vy *= -1
         screen.blit(bb_img, bb_rct)
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
